37671ec34fbe3917a2c2f639a5abe665e83d4403
DmitriyDemintsev <d.a.demintsev@yandex.ru>
2024-10-19 11:47:35 +0300

    Initial commit

---
+++ src/main/java/com/example/test_for_VitaSoft/TestForVitaSoftApplication.java
@@ -0,0 +1,12 @@
+package com.example.test_for_VitaSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestForVitaSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestForVitaSoftApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/test_for_VitaSoft/TestForVitaSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_for_VitaSoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestForVitaSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
